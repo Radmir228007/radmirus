8048c5543bfaa88beb25a8a09d4df372b5842dfb
Shafeev Radmir <radmir20177@mail.ru>
2019-11-03 14:58:57 +0300

    представляю практическую работу

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double sum = 0;
+            int a=0,i;
+
+            for (i = -5; i <= 3; i++)
+            {
+                sum = sum + i;
+                a++;
+                Console.WriteLine(i);
+            }
+            Console.WriteLine(sum/a);
+            Console.ReadKey();
+        }
+       
+    }
+}
